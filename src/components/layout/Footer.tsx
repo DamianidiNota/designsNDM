d2e1c9a9e604be991df7d7d115cb29a55b2acb4e
Nota Damianidi <nota.damianidi@atcom.gr>
2026-08-17 15:25:23 +0300

    Prepare for GitHub Pages

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,14 @@
+function Footer(){
+
+    return(
+
+        <>
+            <footer className="footer">
+                <div className="wrapper">
+                    designsNDM footer
+                </div>               
+            </footer>
+        </>
+    )
+}
+export default Footer;
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,14 @@
+function Header(){
+
+    return(
+
+        <>
+            <header className="header">
+                <div className="wrapper">
+                    designsNDM HEader
+                </div>               
+            </header>
+        </>
+    )
+}
+export default Header;
